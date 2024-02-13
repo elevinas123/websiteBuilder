ba9fcb3459fef97b6073eeb00ba4354ec239720c
elevinas123 <elvinas.monstvilas@gmail.com>
2024-02-13 21:30:21 +0200

    fixed the resizing of elements

--- src/functions/handleElementResize.js
+++ src/functions/handleElementResize.js
@@ -1,61 +1,91 @@
 import { produce } from "immer"
 import calculateNewStyle from "./calculateNewStyle"
-import calculateMovement from "./calculateMovement"
 
 export default function handleElementResize(gridMoving, allElements, setGridMoving, setAllElements) {
     let { top, left, width, height } = allElements[gridMoving.id]
-    console.log(top, left)
-    console.log("top, left", top, left)
+    let deltaX = gridMoving.x2 - gridMoving.x1
+    let deltaY = gridMoving.y2 - gridMoving.y1
+    // Handle creating and resizing-1
+    if (gridMoving.type === "creating") {
+        width += deltaX
+        height += deltaY
+    } else if (gridMoving.type === "resizing-1") {
+        top += deltaY
+        height -= deltaY
+        left += deltaX
+        width -= deltaX
+    }
+    // Implement resizing for the rest of the directions
+    else if (gridMoving.type === "resizing-5") {
+        top += deltaY
+        height -= deltaY
+    } else if (gridMoving.type === "resizing-2") {
+        top += deltaY
+        height -= deltaY
+        width += deltaX
+    } else if (gridMoving.type === "resizing-6") {
+        width += deltaX
+    } else if (gridMoving.type === "resizing-3") {
+        width += deltaX
+        height += deltaY
+    } else if (gridMoving.type === "resizing-7") {
+        height += deltaY
+    } else if (gridMoving.type === "resizing-4") {
+        left += deltaX
+        width -= deltaX
+        height += deltaY
+    } else if (gridMoving.type === "resizing-8") {
+        left += deltaX
+        width -= deltaX
+    }
 
-    let [newWidth, newHeight] = calculateMovement(gridMoving, width, height)
-    console.log("newasda", newWidth, newHeight)
+    // Prevent negative dimensions
 
-    let newStyle = calculateNewStyle(left, top, newWidth, newHeight)
-    console.log(newStyle)
+    let newStyle = calculateNewStyle(left, top, width, height)
+
     if (gridMoving.moved === true) {
-        setGridMoving({ moving: false })
-        if (newHeight < 0) {
-            top +=newHeight
-            newHeight *= -1;
+        if (height < 0) {
+            top += height
+            height *= -1
         }
-        if (newWidth < 0) {
-            top += newWidth
-            newWidth *= -1
+        if (width < 0) {
+            left += width
+            width *= -1
         }
+        newStyle = calculateNewStyle(left, top, width, height)
         setAllElements((currentState) =>
             produce(currentState, (draft) => {
-                // Update the parent element
                 const element = draft[gridMoving.id]
                 if (element) {
-                    element.height = newHeight
-                    element.width = newWidth
+                    element.height = height
+                    element.width = width
                     element.top = top
                     element.left = left
                     element.style = {
                         ...element.style,
                         ...newStyle,
-                        gridTemplateColumns: `repeat(${newWidth}, 1px)`, // 10 columns, each 4px wide
-                        gridTemplateRows: `repeat(${newHeight}, 1px)`,
+                        gridTemplateColumns: `repeat(${width}, 1px)`,
+                        gridTemplateRows: `repeat(${height}, 1px)`,
                     }
                 }
             })
         )
+        setGridMoving({ moving: false })
         return
     }
     setAllElements((currentState) =>
         produce(currentState, (draft) => {
-            // Update the parent element
             const element = draft[gridMoving.id]
             if (element) {
-                element.height = newHeight
-                element.width = newWidth
+                element.height = height
+                element.width = width
                 element.top = top
                 element.left = left
                 element.style = {
                     ...element.style,
                     ...newStyle,
-                    gridTemplateColumns: `repeat(${newWidth}, 1px)`, // 10 columns, each 4px wide
-                    gridTemplateRows: `repeat(${newHeight}, 1px)`,
+                    gridTemplateColumns: `repeat(${width}, 1px)`,
+                    gridTemplateRows: `repeat(${height}, 1px)`,
                 }
             }
         })
